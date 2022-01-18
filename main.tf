6763112b3fbc4817ec51c0fd2aa6228f92e0506a
shrapk2 <16600691+shrapk2@users.noreply.github.com>
2022-01-18 13:26:12 -0600

    Update main.tf
    removing istio here, laying it down via other methods

--- main.tf
+++ main.tf
@@ -21,8 +21,6 @@ resource "null_resource" "cluster" {
   provisioner "local-exec" {
     command = <<-EOT
       k3d cluster create ${each.key} --servers ${var.server_count} --agents ${var.agent_count} --api-port ${var.k3d_cluster_ip}:${var.k3d_cluster_port} --port 80:80@loadbalancer --port 443:443@loadbalancer --k3s-arg '--no-deploy=traefik@server:0'
-    istioctl install -y --set profile=default --context k3d-${each.key}
-    kubectl label namespace default istio-injection=enabled --context k3d-${each.key}
     EOT
   }
 }
